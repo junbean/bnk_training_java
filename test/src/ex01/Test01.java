fc4e0598a34c96bc5942bd89005cec340b327feb
GGG <GGG@DESKTOP-8N3GG4O>
2025-03-06 16:14:29 +0900

    학습내용 - 변수와 자료형

---
+++ test/src/ex01/Test01.java
@@ -0,0 +1,58 @@
+package ex01;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// 최초 실행
+		/*
+		System.out.println("Hello JAVA!");
+		System.out.print("Hello JAVA! - ");
+		System.out.println("Hello JAVA!");
+		*/
+		
+		
+		// 자료와 변수명
+		/*
+		int a, b, c;
+		a = 1;
+		b = 200000000;
+		c = a + b;
+		System.out.println("c의 값은 " + c + "입니다.");	// 출력
+		*/
+		
+		
+		// 짝수 판별 
+		int a;
+		int b;
+		
+		a = 2;
+		b = 1;
+		
+		
+	}
+}
+/*
+
+자료현 정리
+
+char(character)
+	문자를 나타냄
+	홀따옴표로 나타냄
+String 
+	문자열을 나타냄
+	쌍따옴표로 나타냄
+	클래스로 만들어져서 대문자로 나타냄
+
+INT(integer)
+	정수를 나타냄
+double
+	실수를 나타냄
+float
+	실수를 나타냄(10.0f)
+
+
+
+램
+	주기억장치
+	
+*/
